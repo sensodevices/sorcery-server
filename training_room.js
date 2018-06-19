05189d2fc616b244d669ead1aee45c2c91a74949
Sergey Birukov <sergey@senso.me>
2018-06-19 18:40:20 +0700

    Enemy spawning. Spells sync.

--- training_room.js
+++ training_room.js
@@ -1,71 +1,129 @@
 var Room = require('colyseus').Room;
 
+const SpawnEnemyEvery = 15000; 
+const EnemyMaxCount = 0;
+const PlayerMaxCount = 3;
+
 class TrainingRoom extends Room {
 
-  constructor () {
-    super();
+	constructor () {
+		super();
 
-	this.playerSpawnPoints = [
-		{x: -3.0, y: 0.0, z: 2.5}, {x: 3.0, y: 0.0, z: 2.5}
-	];
-	this.prevPlayerSpawnInd = -1;
-	this.enemySpawnPoints = [
-		[-3.0, 0.0, -2.5], [3.0, 0.0, -2.5], [0.0, 0.0, -2.5]
-	];
-	this.prevEnemySpawnInd = -1;
-	
-    this.setState({
-      players: {},
-      messages: []
-    });
-  }
+		this.playerSpawnPoints = [
+			{pos: [-3.0, 0.0, -2.5], rot: [0.0, 0.0, 0.0]}, 
+			{pos: [3.0, 0.0, -2.5], rot: [0.0, 0.0, 0.0]}
+		];
+		this.prevPlayerSpawnInd = -1;
+		this.enemySpawnPoints = [
+			{pos: [-3.0, 0.0, 2.5], rot: [0.0, 0.0, 0.0]},
+			{pos: [3.0, 0.0, 2.5], rot: [0.0, 0.0, 0.0]},
+			{pos: [0.0, 0.0, 2.5], rot: [0.0, 0.0, 0.0]}
+		];
+		this.prevEnemySpawnInd = -1;
+		this.m_spawnTimeout = 5000;
+		this.m_enemiesSpawned = 0;
+		this.m_playerJoined = 0;
 
-  onInit (options) {
-    this.setPatchRate( 1000 / 20 );
-    this.setSimulationInterval( this.update.bind(this) );
-
-    console.log("TrainingRoom created!", options);
-  }
-
-  requestJoin (options) {
-    console.log("request join!", options);
-    return true;
-  }
-
-  onJoin (client) {
-    console.log("client joined!", client.sessionId);
-	let spawnPointInd = this.prevPlayerSpawnInd + 1;
-	if (spawnPointInd >= this.playerSpawnPoints.length) {
-		spawnPointInd = 0;
+		this.setState({
+			players: {},
+			messages: [],
+			enemies: {}
+		});
 	}
-	this.prevPlayerSpawnInd = spawnPointInd;
-    this.state.players[client.sessionId] = this.playerSpawnPoints[spawnPointInd];
-  }
 
-  onLeave (client) {
-    console.log("client left!", client.sessionId);
-    delete this.state.players[client.sessionId];
-  }
+	onInit (options) 
+	{
+		this.setPatchRate( 1000 / 20 );
+		this.setSimulationInterval( this.update.bind(this) );
+		console.log("TrainingRoom created!", options);
+	}
 
-  onMessage (client, data) {
-    console.log(data, "received from", client.sessionId);
-    this.state.messages.push(client.sessionId + " sent " + data);
+	requestJoin (options) 
+	{
+		console.log("request join!", options);
+		if (this.m_playerJoined >= PlayerMaxCount) {
+			console.log("The room is full!");
+			return false;
+		}
+		return true;
+	}
 
-    this.broadcast({hello: "hello world"});
-  }
+	onJoin (client) 
+	{
+		console.log("client joined!", client.id);
+		var pl = this.getPlayerSpawnPoint();
+		this.state.players[client.id] = pl;
+		++this.m_playerJoined;
+	}
 
-  update () {
-    //console.log("num clients:", Object.keys(this.clients).length);
-    for (var sessionId in this.state.players) {
-		console.log(sessionId + " x: " + this.state.players[sessionId].x);
-		this.state.players[sessionId].x += 0.01;
-    }
-  }
+	onLeave (client) 
+	{
+		//console.log("client left!", client);
+		delete this.state.players[client.id];
+	}
 
-  onDispose () {
-    console.log("Dispose TrainingRoom");
-  }
+	onMessage (client, data) 
+	{
+		console.log(data, "received from", client.id);
+		//this.state.messages.push(client.id + " sent " + data);
+		data.clientId = client.id;
+		for (var ind in this.clients)
+		{
+			//if (this.clients[ind].id != client.id)
+			{
+				this.send(this.clients[ind], data);
+			}
+		}
+		
+	}
 
+	update () 
+	{
+		this.m_spawnTimeout -= this.clock.deltaTime;
+		if (this.m_spawnTimeout <= 0)
+		{
+			if (this.m_enemiesSpawned < EnemyMaxCount)
+			{
+				console.log("spawn an enemy");
+				++this.m_enemiesSpawned;
+				var newEnemy = this.getEnemySpawnPoint();
+				newEnemy.health = 100.0;
+				this.state.enemies[this.getEnemyId()] = newEnemy;
+			}
+			this.m_spawnTimeout = SpawnEnemyEvery;
+		}
+		//console.log("num clients:", Object.keys(this.clients).length);
+		//for (var clientId in this.state.players) 
+		//{
+		//	this.state.players[clientId].pos[0] += 0.01;// = newPos;
+		//}
+	}
+
+	onDispose () 
+	{
+		console.log("Dispose TrainingRoom");
+	}
+
+	getPlayerSpawnPoint ()
+	{
+		let ind = this.prevPlayerSpawnInd + 1;
+		if (ind >= this.playerSpawnPoints.length) ind = 0;
+		this.prevPlayerSpawnInd = ind;
+		return this.playerSpawnPoints[ind];
+	}
+  
+	getEnemySpawnPoint () 
+	{
+		var ind = this.prevEnemySpawnInd + 1;
+		if (this.prevEnemySpawnInd >= this.enemySpawnPoints.length) ind = 0;
+		this.prevEnemySpawnInd = ind;
+		return this.enemySpawnPoints[ind];
+	}
+	
+	getEnemyId () 
+	{
+		return Math.random().toString(16).slice(-6);
+	}
 }
 
 module.exports = TrainingRoom;
